93c983539fdc8124871cc0f4c741b402ed9b6ecc
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2023-09-19 10:57:18 +0530

    Added solution - LeetHub

---
+++ Find first set bit - GFG/find-first-set-bit.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    
+    
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();//taking testcases
+		while(t-->0){
+			int n=sc.nextInt();//input n
+			Solution ob=new Solution();
+			System.out.println(ob.getFirstSetBit(n));//calling method
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find position of first set bit in the given number.
+    public static int getFirstSetBit(int n){
+            
+        StringBuilder sb = new StringBuilder(Integer.toBinaryString(n));
+        int index = sb.lastIndexOf("1");
+        if(index<0) return 0;
+        return sb.length()-index;
+            
+    }
+}
