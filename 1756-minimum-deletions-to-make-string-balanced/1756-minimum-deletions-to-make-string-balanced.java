6c61e06479c173d89671d562f64cbbc01a62885e
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2024-07-30 10:48:46 +0530

    Time: 138 ms (6.41%), Space: 45.3 MB (77.16%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
@@ -1,18 +1,32 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int n = s.length();
-        int countB = 0;
-        int remove = 0;
-        for(int i=0;i<n;i++){
-            if(s.charAt(i) == 'a'){
-                if(countB > 0){
-                    countB--;
-                    remove++;
-                }
+        // Approach 1
+        // int n = s.length();
+        // int countB = 0;
+        // int remove = 0;
+        // for(int i=0;i<n;i++){
+        //     if(s.charAt(i) == 'a'){
+        //         if(countB > 0){
+        //             countB--;
+        //             remove++;
+        //         }
+        //     }else{
+        //         countB++;
+        //     }
+        // }
+        // return remove;
+
+        //Approach 2: Using Stack
+        Stack<Character> st = new Stack<>();
+        int res = 0;
+        for(int i=0;i<s.length();i++){
+            if(!st.isEmpty() && st.peek() == 'b' && s.charAt(i) == 'a'){
+                st.pop();
+                res++;
             }else{
-                countB++;
+                st.add(s.charAt(i));
             }
         }
-        return remove;
+        return res;
     }
 }
