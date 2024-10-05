c6750c5a33e61d428af7e775ef2de53bf0d2e095
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2024-10-05 17:40:39 +0530

    Time: 1 ms (83.26%), Space: 41.3 MB (81.29%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,15 +1,28 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
+        // In two traversal
+        // int arr[] = new int[26];
+        // for(char ch : sentence.toCharArray()){
+        //     int index = ch - 'a';
+        //     arr[index]++;
+        // }
+        // for(int count : arr){
+        //     if(count == 0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        //In one traversal
         int arr[] = new int[26];
+        int count = 0;
         for(char ch : sentence.toCharArray()){
             int index = ch - 'a';
-            arr[index]++;
-        }
-        for(int count : arr){
-            if(count == 0){
-                return false;
+            if(arr[index] == 0){
+                arr[index]++;
+                count++;
             }
         }
-        return true;
+        return count == 26;
     }
 }
