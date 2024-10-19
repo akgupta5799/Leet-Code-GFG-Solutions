bd01ee4786f8f33aa351327fc8f933632f095790
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2024-10-19 16:57:39 +0530

    Time: 0 ms (100%), Space: 42.2 MB (20.96%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int pivot_index = findPivot(nums,n);// pivot element is minimum element in rotated sorted array same as Leetcode 153
+        int idx = binarySearch(nums, target, 0, pivot_index - 1); // left side of pivot_index
+        if(idx != -1){
+            return idx;
+        }
+
+        idx = binarySearch(nums, target, pivot_index, n-1); //right side including pivot_index
+        return idx;
+    }
+
+    public static int findPivot(int[] nums, int n){
+        int l = 0;
+        int r = n - 1;
+        while(l < r){
+            int mid = l + (r - l)/2;
+            if(nums[mid] > nums[r]){
+                l = mid + 1;
+            }else{
+                r = mid;
+            }
+        }
+        return r;
+    }
+
+    public static int binarySearch(int[] nums, int target, int l, int r){
+        int idx = -1;
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            if(nums[mid] == target){
+                idx = mid;
+                break;
+            }else if(nums[mid] < target){
+                l = mid + 1;
+            }else{
+                r = mid - 1;
+            }
+        }
+        return idx;
+    }
+}
