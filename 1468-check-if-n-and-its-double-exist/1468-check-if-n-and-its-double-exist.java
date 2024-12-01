f1045f1e1ab5fd2b3df1501136515d59b7b7bc23
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2024-12-01 13:02:18 +0530

    Time: 5 ms (8.99%), Space: 44.1 MB (7.1%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -1,22 +1,28 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                if(i!=j && arr[i] == 2*arr[j]){
-                    return true;
-                }
-            }
-        }
-        return false;
-        // HashMap<Integer, Integer> map = new HashMap<>();
+        // Brute Force O(n^2)
         // for(int i=0;i<arr.length;i++){
-        //     map.put(arr[i],i);
-        // }
-        // for(int i=0;i<arr.length;i++){
-        //     if(map.containsKey(2*arr[i])){
-        //         return true;
+        //     for(int j=0;j<arr.length;j++){
+        //         if(i!=j && arr[i] == 2*arr[j]){
+        //             return true;
+        //         }
         //     }
         // }
         // return false;
+
+        // O(n)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i], map.getOrDefault(arr[i],0)+1);
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] != 0 && map.containsKey(2*arr[i])){
+                return true;
+            }
+            if(arr[i] == 0 && map.get(arr[i]) > 1){
+                return true;
+            }
+        }
+        return false;
     }
 }
