f8259169e310ca6dad003423e8874777edf0209f
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2024-12-03 14:30:42 +0530

    Time: 24 ms (58.36%), Space: 78.2 MB (93.43%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.java
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.java
@@ -1,21 +1,34 @@
 class Solution {
     public String addSpaces(String s, int[] spaces) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<spaces.length;i++){
-            set.add(spaces[i]);
-        }
+        // What I though (Bekar)
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int i=0;i<spaces.length;i++){
+        //     set.add(spaces[i]);
+        // }
+
+        // StringBuilder sb = new StringBuilder();
+        // int n = s.length();
+        // int i = 0;
+        // while(i < n){
+        //     if(!set.contains(i)){
+        //         sb.append(s.charAt(i));
+        //     }else{
+        //         sb.append(" ");
+        //         sb.append(s.charAt(i));
+        //     }
+        //     i++;
+        // }
+        // return sb.toString();
 
         StringBuilder sb = new StringBuilder();
         int n = s.length();
-        int i = 0;
-        while(i < n){
-            if(!set.contains(i)){
-                sb.append(s.charAt(i));
-            }else{
+        int spaceIndex = 0;
+        for(int i=0;i<n;i++){
+            if(spaceIndex < spaces.length && i == spaces[spaceIndex]){
                 sb.append(" ");
-                sb.append(s.charAt(i));
+                spaceIndex++;
             }
-            i++;
+            sb.append(s.charAt(i));
         }
         return sb.toString();
     }
