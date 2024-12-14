aab6f94de35cf572c9aaa0e761404cc52f17cb20
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2024-12-14 22:12:05 +0530

    Time: 0 ms (100%), Space: 42.5 MB (6.15%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,21 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
-        int pivot_index = findPivot(nums, n);
-       
-        int idx = binarySearch(nums, target, 0, pivot_index-1);
+        int pivot_index = findPivot(nums, n);// min value index
+        int idx = binarySearch(nums, target, 0, pivot_index -1);//left side
         if(idx != -1){
             return idx;
         }
-        idx = binarySearch(nums, target, pivot_index, n-1);
+        idx = binarySearch(nums, target, pivot_index, n-1);// right side
         return idx;
-        
     }
     public static int findPivot(int[] nums, int n){
         int l = 0;
         int r = n-1;
-        while(l < r){
-            int mid = l + (r - l)/2;
+        while(l <r ){
+            int mid = l+(r-l)/2;
             if(nums[mid] > nums[r]){
                 l = mid + 1;
             }else{
@@ -28,7 +26,7 @@ class Solution {
     public static int binarySearch(int[] nums, int target, int l, int r){
         int idx = -1;
         while(l <= r){
-            int mid = l + (r -l)/2;
+            int mid = l + (r - l)/2;
             if(nums[mid] == target){
                 idx = mid;
                 break;
