117b5c24e2aa806cc38d5a03562c1e2f09ae31ea
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2024-12-27 00:36:25 +0530

    Time: 2 ms (98.93%), Space: 45.3 MB (6.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -9,6 +9,7 @@ class Solution {
             if(map.containsKey(target - nums[i])){
                 res[0] = i;
                 res[1] = map.get(target - nums[i]);
+                break;
             }else{
                  map.put(nums[i], i);
             } 
