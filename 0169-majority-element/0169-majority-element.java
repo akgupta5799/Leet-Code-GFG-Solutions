5d33fc0da31fb55837b060292aef96ce076371d4
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2025-01-18 19:43:24 +0530

    Time: 12 ms (36.41%), Space: 49.9 MB (72.43%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,37 +1,17 @@
-class Solution { 
+class Solution {
     public int majorityElement(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
 
-        // Low beats 38%
-
-        // HashMap<Integer, Integer> map = new HashMap<>();
-        // for (int i = 0; i < nums.length; i++) {
-        //     map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        // }
-        // for (int x : map.keySet()) {
-        //     if (map.get(x) > nums.length / 2) {
-        //         return x;
-        //     }
-        // }
-        // return -1;
-
-        // 66%
-        // Arrays.sort(nums);
-        // return nums[nums.length/2];
-
-        /*Boyer-Moore Voting Algorithm */
-        int maj = 0;
-        int count = 0;
-        for (int num : nums) {
-            if (count == 0) {
-                maj = num;
-                count = 1;
-            } else if (maj == num) {
-                count++;
-            } else {
-                count--;
+        for(int x : map.keySet()){
+            if(map.get(x) > n/2){
+                return x;
             }
         }
-        // Here we are not doing verfiation part because majority element always exists in the array.
-        return maj;
+        return -1;
+
     }
 }
