09ba607fb4ff9922f53045f3d108dc8a348659cd
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2025-01-23 10:56:29 +0530

    Time: 2 ms (99.26%), Space: 48.6 MB (11.26%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,45 +3,46 @@ class Solution {
         /* Boyer-Moore Voting Algorithm */
         // Assumption Part
         int n = nums.length;
-        Integer maj1 = null;
-        Integer maj2 = null;
+        int majority1 = 0;
+        int majority2 = 0;
         int count1 = 0;
         int count2 = 0;
         for(int i=0;i<n;i++){
-            if(maj1 != null && nums[i] == maj1){
+            if(majority1 == nums[i]){
                 count1++;
-            }else if(maj2 != null && nums[i] == maj2){
+            }else if(majority2 == nums[i]){
                 count2++;
             }else if(count1 == 0){
-                maj1 = nums[i];
+                majority1 = nums[i];
                 count1 = 1;
             }else if(count2 == 0){
-                maj2 = nums[i];
+                majority2 = nums[i];
                 count2 = 1;
             }else{
                 count1--;
                 count2--;
             }
         }
+
         // Verification Part
         List<Integer> res = new ArrayList<>();
         int freq1 = 0;
         int freq2 = 0;
         for(int i=0;i<n;i++){
-            if(maj1 != null && nums[i] == maj1){
+            if(nums[i] == majority1){
                 freq1++;
-            }else if(maj2 != null && nums[i] == maj2){
+            }else if(nums[i] == majority2){
                 freq2++;
             }
         }
 
-        if(freq1 > Math.floor(n/3)){
-            res.add(maj1);
+        if(freq1 > n/3){
+            res.add(majority1);
         }
-        if(freq2 > Math.floor(n/3)){
-            res.add(maj2);
+        if(freq2 > n/3){
+            res.add(majority2);
         }
-
         return res;
+        
     }
 }
