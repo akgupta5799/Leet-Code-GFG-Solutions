082f2b03ac3e80b35a2296a886e425064b27a5b3
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2025-01-23 10:53:26 +0530

    Time: 1 ms (99.81%), Space: 53.5 MB (14.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,33 +1,38 @@
-class Solution {
+class Solution { 
     public int majorityElement(int[] nums) {
-        // int n = nums.length;
-        // HashMap<Integer, Integer> map = new HashMap<>();
-        // for(int i=0;i<n;i++){
-        //     map.put(nums[i], map.getOrDefault(nums[i],0)+1);
-        // }
 
-        // for(int x : map.keySet()){
-        //     if(map.get(x) > n/2){
+        // Low beats 38%
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for (int i = 0; i < nums.length; i++) {
+        //     map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        // }
+        // for (int x : map.keySet()) {
+        //     if (map.get(x) > nums.length / 2) {
         //         return x;
         //     }
         // }
         // return -1;
 
+        // 66%
+        // Arrays.sort(nums);
+        // return nums[nums.length/2];
+
+        /*Boyer-Moore Voting Algorithm */
         int n = nums.length;
-        int majority = 0;
+        int maj = 0;
         int count = 0;
         for(int i=0;i<n;i++){
-            if(count == 0){
-                majority = nums[i];
-                count = 1;
-            }else if(majority == nums[i]){
+            if(maj == nums[i]){
                 count++;
+            }else if(count == 0){
+                maj = nums[i];
+                count = 1;
             }else{
                 count--;
             }
         }
-
-        return majority;
-
+        // Here we are not doing verfiation part because majority element always exists in the array.
+        return maj;
     }
 }
