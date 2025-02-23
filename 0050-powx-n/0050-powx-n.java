39768e4dc8c40f6e4518537618e5c5f5dbbcd22f
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2025-02-23 19:43:02 +0530

    Time: 0 ms (100%), Space: 43.3 MB (19.08%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -7,6 +7,6 @@ class Solution {
         if(n == 0) return 1;
         if(n < 0) return solve(1/x, -n);
         if(n % 2 == 0) return solve(x*x, n/2);
-        return x * solve(x*x, (n-1)/2);
+        return x*solve(x*x, (n-1)/2);
     }
 }
