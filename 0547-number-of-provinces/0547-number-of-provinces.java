2212df2d84543fb19e7bbf4f058b8bd842f62028
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2025-03-16 20:51:04 +0530

    Time: 5 ms (13.04%), Space: 47.1 MB (80.84%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,4 +1,40 @@
-class Solution {
+// Using DFS
+// class Solution {
+//     public int findCircleNum(int[][] isConnected) {
+//         int n = isConnected.length;
+//         HashMap<Integer, List<Integer>> adj = new HashMap<>();
+//         for(int i=0;i<n;i++){
+//             adj.putIfAbsent(i, new ArrayList<>());
+//             for(int j=0;j<n;j++){
+//                 if(isConnected[i][j] == 1){
+//                     adj.get(i).add(j);
+//                 }
+//             }
+//         }
+//         boolean visited[] = new boolean[n];
+//         int count = 0;
+//         for(int i=0;i<n;i++){
+//             if(!visited[i]){
+//                 dfs(adj, i, visited);
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+//     public static void dfs(HashMap<Integer, List<Integer>> adj, int node, boolean visited[]){
+//         visited[node] = true;
+//         for(Integer it : adj.get(node)){
+//             if(!visited[it]){
+//                 dfs(adj, it, visited);
+//             }
+//         }
+//     }
+
+// }
+
+
+// Using BFS
+class Solution{
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         HashMap<Integer, List<Integer>> adj = new HashMap<>();
@@ -10,23 +46,30 @@ class Solution {
                 }
             }
         }
+
         boolean visited[] = new boolean[n];
         int count = 0;
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                dfs(adj, i, visited);
+                bfs(adj, visited, i);
                 count++;
             }
         }
         return count;
     }
-    public static void dfs(HashMap<Integer, List<Integer>> adj, int node, boolean visited[]){
+
+    public static void bfs(HashMap<Integer, List<Integer>> adj, boolean visited[], int node){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(node);
         visited[node] = true;
-        for(Integer it : adj.get(node)){
-            if(!visited[it]){
-                dfs(adj, it, visited);
+        while(!queue.isEmpty()){
+            Integer u = queue.poll();
+            for(Integer v : adj.get(u)){
+                if(!visited[v]){
+                    queue.add(v);
+                    visited[v] = true;
+                }
             }
         }
     }
-
 }
