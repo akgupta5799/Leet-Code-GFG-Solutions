bf383f13ffe6fdcde91bebe14f93871edf6eedf0
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2025-04-08 17:23:30 +0530

    Time: 0 ms (100%), Space: 40.5 MB (75.06%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -1,11 +1,14 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        int n = start ^ goal;
-        int count = 0;
-        while(n != 0){
-            n = n & (n-1);
-            count++;
-        }
-        return count;
+        // int n = start ^ goal;
+        // int count = 0;
+        // while(n != 0){
+        //     n = n & (n-1);
+        //     count++;
+        // }
+        // return count;
+
+        // In one line
+        return Integer.bitCount(start ^ goal);
     }
 }
