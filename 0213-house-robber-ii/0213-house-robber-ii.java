16143f1542f8217e5a31159553d1b8971bea6f27
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2025-05-18 22:25:34 +0530

    Time: 0 ms (100%), Space: 41.1 MB (50.93%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -17,12 +17,14 @@ class Solution {
         if(arr.length==1){
             return arr[0];
         }
-        int dp[] = new int[arr.length];
-        dp[0] = arr[0];
-        dp[1] = Math.max(arr[0],arr[1]);
-        for(int i=2;i<arr.length;i++){
-            dp[i] = Math.max(dp[i-2]+arr[i], dp[i-1]);
+        int dp[] = new int[arr.length+1];
+        dp[0] = 0;
+        dp[1] = arr[0];
+        for(int i=2;i<arr.length+1;i++){
+            int steal = arr[i-1] + dp[i-2];
+            int skip = dp[i-1];
+            dp[i] = Math.max(steal, skip);
         }
-        return dp[arr.length-1];
+        return dp[arr.length];
     }
 }
