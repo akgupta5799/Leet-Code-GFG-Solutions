0d7dd4b02920d32ebbe7a979cc0be89df8249a03
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2025-07-08 19:42:05 +0530

    Time: 5 ms (55.36%), Space: 68.7 MB (38.21%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,21 +10,52 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ArrayList<Integer> list = new ArrayList<>();   
+        // ArrayList<Integer> list = new ArrayList<>();   
+        // ListNode curr = head;
+        // while(curr != null){
+        //     list.add(curr.val);
+        //     curr = curr.next;
+        // }
+        // int l = 0;
+        // int r = list.size() - 1;
+        // while(l < r){
+        //     if(list.get(l) != list.get(r)){
+        //         return false;
+        //     }
+        //     l++;
+        //     r--;
+        // }
+        // return true;
+        ListNode mid = middle(head);
+        ListNode tail = reverse(mid);
         ListNode curr = head;
-        while(curr != null){
-            list.add(curr.val);
-            curr = curr.next;
-        }
-        int l = 0;
-        int r = list.size() - 1;
-        while(l < r){
-            if(list.get(l) != list.get(r)){
+        while(tail != null){
+            if(curr.val != tail.val){
                 return false;
             }
-            l++;
-            r--;
+            curr = curr.next;
+            tail = tail.next;
         }
         return true;
+    }
+    public static ListNode middle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public static ListNode reverse(ListNode head){
+        ListNode prev = null, curr = head, next = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+
     }
 }
