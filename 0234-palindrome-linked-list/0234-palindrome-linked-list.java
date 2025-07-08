629663c03ffeaf5afd1e327f30f8cf8dd6acadcc
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2025-07-08 14:45:41 +0530

    Time: 7 ms (38.24%), Space: 55.8 MB (99.22%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,49 +10,66 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode mid = middle(head); // finding middle node
-        ListNode tail = reverse(mid); // reversing second half of linked list
+        ArrayList<Integer> list = new ArrayList<>();
         ListNode curr = head;
-
-        while(tail!=null){
-            if(curr.val != tail.val){
-                return false;
-            }
-            tail = tail.next;
+        while(curr != null){
+            list.add(curr.val);
             curr = curr.next;
         }
+        int left = 0;
+        int right = list.size()-1;
+        while(left < right){
+            if(list.get(left) != list.get(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
         return true;
+    //     ListNode mid = middle(head); // finding middle node
+    //     ListNode tail = reverse(mid); // reversing second half of linked list
+    //     ListNode curr = head;
+
+    //     while(tail!=null){
+    //         if(curr.val != tail.val){
+    //             return false;
+    //         }
+    //         tail = tail.next;
+    //         curr = curr.next;
+    //     }
+    //     return true;
     }
     
-    // Function for finding the middle node
-    public ListNode middle(ListNode head){
-        ListNode slow = head;
-        ListNode fast = head;
+    // // Function for finding the middle node
+    // public ListNode middle(ListNode head){
+    //     ListNode slow = head;
+    //     ListNode fast = head;
 
-        while(fast!=null && fast.next!=null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
+    //     while(fast!=null && fast.next!=null){
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+    //     }
 
-        // if the number of nodes are odd then definitely fast pointer will be at the last node
-        // if(fast!=null){
-        //     return slow.next;
-        // }
-        return slow;
-    }
+    //     // if the number of nodes are odd then definitely fast pointer will be at the last node
+    //     if(fast!=null){
+    //         return slow.next;
+    //     }
+    //     return slow;
+    // }
 
-    // Function for finding reverse from the mid node 
-    public ListNode reverse(ListNode head){
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode next = null;
+    // // Function for finding reverse from the mid node 
+    // public ListNode reverse(ListNode head){
+    //     ListNode prev = null;
+    //     ListNode curr = head;
+    //     ListNode next = null;
 
-        while(curr!=null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
+    //     while(curr!=null){
+    //         next = curr.next;
+    //         curr.next = prev;
+    //         prev = curr;
+    //         curr = next;
+    //     }
+    //     return prev;
+    // }
+    
 }
