982dcc33c403fcd5f8393cfbb4392b50f64d3aca
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2025-07-15 11:51:57 +0530

    Time: 1 ms (99.12%), Space: 41.8 MB (97.13%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String word) {
+        int n = word.length();
+        if(n < 3) return false;
+        int vowel = 0;
+        int consonant = 0;
+        for(char ch : word.toCharArray()){
+            if(Character.isLetter(ch)){
+                if("aeiouAEIOU".indexOf(ch) != -1){
+                    vowel++;
+                }else{
+                    consonant++;
+                }
+            }else if(!Character.isDigit(ch)){
+                return false;
+            }
+        }
+        if(vowel >= 1 && consonant >= 1){
+            return true;
+        }
+        return false;
+    }
+}
