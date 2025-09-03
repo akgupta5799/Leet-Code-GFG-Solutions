c0d94cf895bad5f538724cfffe9002f895f1efe5
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2025-09-04 00:50:34 +0530

    Time: 5 ms (15.64%), Space: 43.3 MB (81.39%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,16 +1,40 @@
+// class Solution {
+//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+           
+//         Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
+//         Stack<Integer> stack = new Stack<>();
+//         for (int num : nums2) {
+//             while (!stack.isEmpty() && stack.peek() < num)
+//                 map.put(stack.pop(), num);
+//             stack.push(num);
+//         }   
+//         for (int i = 0; i < nums1.length; i++)
+//             nums1[i] = map.getOrDefault(nums1[i], -1);
+//         return nums1;
+//     }
+    
+// }
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-           
-        Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
-        Stack<Integer> stack = new Stack<>();
-        for (int num : nums2) {
-            while (!stack.isEmpty() && stack.peek() < num)
-                map.put(stack.pop(), num);
-            stack.push(num);
-        }   
-        for (int i = 0; i < nums1.length; i++)
-            nums1[i] = map.getOrDefault(nums1[i], -1);
-        return nums1;
+        int[] res = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            int target = nums1[i];
+            int ng = -1;
+            boolean found = false;
+
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums2[j] == target) {
+                    found = true;
+                } else if (found && nums2[j] > target) {
+                    ng = nums2[j];
+                    break;
+                }
+            }
+            res[i] = ng;
+        }
+
+        return res;
     }
-    
 }
