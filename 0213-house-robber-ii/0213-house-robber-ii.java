f0bd69acb02d73fec6047e295b4340ae006680d7
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2026-01-01 23:50:47 +0530

    Time: 0 ms (100%), Space: 43 MB (7.49%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,30 +1,28 @@
 class Solution {
-    public int rob(int[] nums) { //nums[] = [1,2,3,1]
-        if(nums.length==1){
-            return nums[0];
-        }
-        int skipFirst[] = new int[nums.length-1]; 
-        int skipLast[] = new int[nums.length-1];  
-        for(int i=0;i<nums.length-1;i++){
-            skipFirst[i] = nums[i+1]; //skipFirst[] = [2,3,1]
-            skipLast[i] = nums[i];    //skiplast[] = [1,2,3]
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int skipFirst[] = new int[n-1];
+        int skipLast[] = new int[n-1];
+        for(int i=0;i<n-1;i++){
+            skipFirst[i] = nums[i+1];
+            skipLast[i] = nums[i];
         }
         int res1 = robHelper(skipFirst);
         int res2 = robHelper(skipLast);
-        return Math.max(res1,res2);
+        return Math.max(res1, res2);
     }
-    public int robHelper(int[] arr){
-        if(arr.length==1){
-            return arr[0];
-        }
-        int dp[] = new int[arr.length+1];
+    public int robHelper(int arr[]){
+        int n = arr.length;
+        if(n == 1) return arr[0];
+        int dp[] = new int[n+1];
         dp[0] = 0;
         dp[1] = arr[0];
-        for(int i=2;i<arr.length+1;i++){
+        for(int i=2;i<n+1;i++){
             int steal = arr[i-1] + dp[i-2];
             int skip = dp[i-1];
             dp[i] = Math.max(steal, skip);
         }
-        return dp[arr.length];
+        return dp[n];
     }
 }
