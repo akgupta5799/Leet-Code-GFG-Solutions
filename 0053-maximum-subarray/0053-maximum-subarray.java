d4bf37de3826cd792162c1c4d0f72ba270140c94
Adarsh Kumar Gupta <103876113+akgupta5799@users.noreply.github.com>
2026-01-24 20:59:26 +0530

    Time: 1 ms (99.6%), Space: 77.4 MB (25.91%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // T.C. O(n^2) TLE
+        // T.C. O(n^2)
         // int n = nums.length;
         // int max = Integer.MIN_VALUE;
         // for(int i=0;i<n;i++){
@@ -14,36 +14,20 @@ class Solution {
         // }
         // return max;
 
-        // T.C. O(n) { Optimal Solution *Kadane's Algorithm* }
+        // Kadane's Algorithm
         int n = nums.length;
         int currSum = nums[0];
         int overAllSum = nums[0];
-        // int start = 0;
-        // int end = 0;
-        // int tempStart = 0;
         for(int i=1;i<n;i++){
             if(currSum >= 0){
                 currSum += nums[i];
             }else{
                 currSum = nums[i];
-                //tempStart = i;
             }
-
             if(currSum > overAllSum){
                 overAllSum = currSum;
-                //start = tempStart;
-                //end = i;
             }
         }
-
-        // Printing result Subarray
-        // int res[] = new int[end - start + 1];
-        // for(int i=start;i<=end;i++){
-        //     res[i-start] = nums[i];
-        // }
-        // for(int val : res){
-        //     System.out.println(val + " ");
-        // }
         return overAllSum;
     }
 }
